0aefbe033a1889db83ad885aceab1634ce1833af
1982moc <1982moc@gmail.com>
2024-06-11 15:01:25 +0300

    454

--- src/main/java/com/example/demospringsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/example/demospringsecurity/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demospringsecurity.config;
 
+import com.example.demospringsecurity.service.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,18 +17,16 @@ import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SuccessUserHandler successUserHandler;
+    private final UserDetailsServiceImpl userDetailsService;
 
-    private UserDetailsService userDetailsService;
-
-    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserDetailsService userDetailsService) {
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
         this.successUserHandler = successUserHandler;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/", "/index", "/login", "/error").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().hasAnyRole("USER", "ADMIN")
@@ -41,8 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedPage("/forbidden");
     }
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
     }
 
